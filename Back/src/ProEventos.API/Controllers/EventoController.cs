1010b241e1d58769567c9f7b80dcd9daf7b393f8
Peter <peterlsouza@gmail.com>
2022-06-29 01:48:26 -0300

    first commit

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+  [ApiController]
+  [Route("api/[controller]")]
+  public class EventoController : ControllerBase
+  {
+    public EventoController()
+    {
+
+    }
+
+    [HttpGet]
+    public IEnumerable<Evento> Get()
+    {
+      return new Evento[]{
+        new Evento(){
+        EventoId = 1,
+        Tema = "Teste",
+        Local = "BH",
+        Lote = "1 lote",
+        QtdPessoas = 300,
+        DataEvento = DateTime.Now.AddDays(30).ToString()
+      }
+    };
+
+  }
+}
+}
